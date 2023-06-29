77b6dceca35c33d0d164820f984004a44965701a
Mark Wanjau <theafrikanson@gmail.com>
2023-06-29 17:08:33 +0300

    update the restaurant contrller to find by and include pizzas

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,3 +2,8 @@ class RestaurantsController < ApplicationController
     def index
         render json: Restaurant.all
 end
+
+    def show
+        restaurant=Restaurant.find_by!(id: params[:id])
+        render json: restaurant, include: :pizzas
+    end
