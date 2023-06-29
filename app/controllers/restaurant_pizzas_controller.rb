21c1d15bbc1b5a66a98ff055eabcfc465fa122b5
Mark Wanjau <theafrikanson@gmail.com>
2023-06-29 17:55:33 +0300

    check if status ok

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,11 @@
 class RestaurantPizzasController < ApplicationController
+    def create
+        RestaurantPizza.create(restaurant_pizza_params)
+    end
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price,:pizza_is,:restaurant_id)
+    end
 end
